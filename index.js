155774ce49c0913b01addf43803d1a4f63333588
Haidar ezio <haidar.ezio2014@gmail.com>
2020-07-20 13:41:13 +0100

    Adding a gallery to each category

--- index.js
+++ index.js
@@ -52,12 +52,107 @@ gallery.querySelectorAll("img").forEach(function (item) {
 window.addEventListener("resize", resizeAll);
 
 //////////////////* GALLERY RESIZING ALGORITHM
+//////////////////? GALLERY RESIZING ALGORITHM 2
 
-//! gallery.querySelectorAll(".gallery-item").forEach(function (item) {
-// !  item.addEventListener("click", function () {
-// !    item.classList.toggle("full");
-// !  });
-// !});
-// !if (gallery.querySelector("full")) {
-// !  item.classList.remove("full");
-//! }
+var popup = document.querySelector("#popup");
+var getValue = function (elem, style) {
+  return parseInt(window.getComputedStyle(elem).getPropertyValue(style));
+};
+var getH = function (item) {
+  return item.querySelector(".popup__content").getBoundingClientRect().height;
+};
+var resize = function () {
+  var altura = getValue(popup, "grid-auto-rows");
+  var gap = getValue(popup, "grid-row-gap");
+  popup.querySelectorAll(".popup__item").forEach(function (item) {
+    var el = item;
+    el.style.gridRowEnd = "span " + Math.ceil((getH(item) + gap) / (altura + gap));
+  });
+};
+popup.querySelectorAll("img").forEach(function (item) {
+  item.classList.add("popup__byebye");
+  if (item.complete) {
+    console.log(item.src);
+  } else {
+    item.addEventListener("load", function () {
+      var altura = getValue(popup, "grid-auto-rows");
+      var gap = getValue(popup, "grid-row-gap");
+      var gitem = item.parentElement.parentElement;
+      gitem.style.gridRowEnd = "span " + Math.ceil((getH(gitem) + gap) / (altura + gap));
+      item.classList.remove("popup__byebye");
+    });
+  }
+});
+window.addEventListener("resize", resize);
+
+//////////////////? GALLERY RESIZING ALGORITHM 2
+//////////////////* GALLERY RESIZING ALGORITHM 3
+function poped() {
+  var poped = document.querySelector("#poped");
+  var getValue = function (elem, style) {
+    return parseInt(window.getComputedStyle(elem).getPropertyValue(style));
+  };
+  var getH = function (item) {
+    return item.querySelector(".poped__content").getBoundingClientRect().height;
+  };
+  var resize = function () {
+    var altura = getValue(poped, "grid-auto-rows");
+    var gap = getValue(poped, "grid-row-gap");
+    poped.querySelectorAll(".poped__item").forEach(function (item) {
+      var el = item;
+      el.style.gridRowEnd = "span " + Math.ceil((getH(item) + gap) / (altura + gap));
+    });
+  };
+  poped.querySelectorAll("img").forEach(function (item) {
+    item.classList.add("poped__byebye");
+    if (item.complete) {
+      console.log(item.src);
+    } else {
+      item.addEventListener("load", function () {
+        var altura = getValue(poped, "grid-auto-rows");
+        var gap = getValue(poped, "grid-row-gap");
+        var gitem = item.parentElement.parentElement;
+        gitem.style.gridRowEnd = "span " + Math.ceil((getH(gitem) + gap) / (altura + gap));
+        item.classList.remove("poped__byebye");
+      });
+    }
+  });
+  window.addEventListener("resize", resize);
+}
+poped();
+//////////////////* GALLERY RESIZING ALGORITHM 3
+//////////////////? GALLERY RESIZING ALGORITHM 4
+function pop() {
+  var pop = document.querySelector("#pop");
+  var getValue = function (elem, style) {
+    return parseInt(window.getComputedStyle(elem).getPropertyValue(style));
+  };
+  var getH = function (item) {
+    return item.querySelector(".pop__content").getBoundingClientRect().height;
+  };
+  var resize = function () {
+    var altura = getValue(pop, "grid-auto-rows");
+    var gap = getValue(pop, "grid-row-gap");
+    pop.querySelectorAll(".pop__item").forEach(function (item) {
+      var el = item;
+      el.style.gridRowEnd = "span " + Math.ceil((getH(item) + gap) / (altura + gap));
+    });
+  };
+  pop.querySelectorAll("img").forEach(function (item) {
+    item.classList.add("pop__byebye");
+    if (item.complete) {
+      console.log(item.src);
+    } else {
+      item.addEventListener("load", function () {
+        var altura = getValue(pop, "grid-auto-rows");
+        var gap = getValue(pop, "grid-row-gap");
+        var gitem = item.parentElement.parentElement;
+        gitem.style.gridRowEnd = "span " + Math.ceil((getH(gitem) + gap) / (altura + gap));
+        item.classList.remove("pop__byebye");
+      });
+    }
+  });
+  window.addEventListener("resize", resize);
+}
+pop();
+//////////////////? GALLERY RESIZING ALGORITHM 4
