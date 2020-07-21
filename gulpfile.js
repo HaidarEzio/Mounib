6f53cde472d8cc8edf0e29a48c387528fccb3f10
Haidar ezio <haidar.ezio2014@gmail.com>
2020-07-21 19:46:27 +0100

    Optimizing the gif in a better way

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require("gulp");
 const imagemin = require("gulp-imagemin");
 
 function imgSquash() {
-  return gulp.src("./public/img/Studio/*").pipe(imagemin()).pipe(gulp.dest("./public/minified/images/Studio"));
+  return gulp.src("./public/img/*").pipe(imagemin()).pipe(gulp.dest("./public/minified/img/"));
 }
 gulp.task("imgSquash", imgSquash);
 gulp.task("watch", () => {
